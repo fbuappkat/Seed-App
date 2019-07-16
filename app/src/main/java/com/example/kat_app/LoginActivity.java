ebfd2fbbac5f8fda588b0822be120b215d6d055a
Katie Mishra <katiemishra@fb.com>
2019-07-16 10:14:52 -0700

    user can login, user is persisted across app closures

---
+++ app/src/main/java/com/example/kat_app/LoginActivity.java
@@ -0,0 +1,99 @@
+package com.example.kat_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+/* FBU 2019
+ * Login allows users to sign in, or sign up for a new account.
+   Accounts are hosted on via Heroku. If a user is already logged in,
+   they are automatically redirected to the TimelineActivity.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+    private Button loginToSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            loginToHome();
+        } else {
+            setContentView(R.layout.activity_main);
+            getSupportActionBar().hide();
+            usernameInput = (EditText) findViewById(R.id.etLoginUsername);
+            passwordInput = (EditText) findViewById(R.id.etLoginPassword);
+            loginBtn = findViewById(R.id.btnLogin);
+            loginToSignup = findViewById(R.id.btnLoginToSignup);
+
+            usernameInput.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    usernameInput.setHint("");
+                }
+            });
+
+            passwordInput.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    passwordInput.setHint("");
+                }
+            });
+
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final String username = usernameInput.getText().toString();
+                    final String password = passwordInput.getText().toString();
+
+                    login(username, password);
+                }
+            });
+
+            loginToSignup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final Intent loginToSignup = new Intent(LoginActivity.this, SignUpActivity.class);
+                    startActivity(loginToSignup);
+                    finish();
+                }
+            });
+        }
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.d("LoginActivity", "Login successful");
+                    loginToHome();
+                    finish();
+                } else {
+                    Log.e("LoginActivity", "Login error");
+                    Toast.makeText(LoginActivity.this, "Login credentials incorrect", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void loginToHome() {
+        final Intent loginToHome = new Intent(LoginActivity.this, TimelineActivity.class);
+        startActivity(loginToHome);
+    }
+}
