08e4991d6d9eea7507abe57fbdfe02e1a9362732
Timi Soetan <timisoetan@fb.com>
2019-07-16 16:44:10 -0700

    Add fonts

--- app/src/main/java/com/example/kat_app/Data/NotificationAdapter.java
+++ app/src/main/java/com/example/kat_app/Data/NotificationAdapter.java
@@ -1,12 +1,18 @@
 package com.example.kat_app.Data;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.kat_app.Model.Notification;
+import com.example.kat_app.R;
 
 import java.util.List;
 
-public class NotificationAdapter {
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
 
     private List<Notification> notifications;
     Context context;
@@ -16,6 +22,25 @@ public class NotificationAdapter {
         this.context = context;
     }
 
+    @Override
+    public NotificationAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_notification, parent, false);
+        final ViewHolder holder = new ViewHolder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(NotificationAdapter.ViewHolder viewHolder, int i) {
 
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
