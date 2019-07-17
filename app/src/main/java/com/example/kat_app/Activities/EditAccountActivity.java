40d4e71e2fbcf4bc0d4aff8165baf5761029f2fe
Timi Soetan <timisoetan@fb.com>
2019-07-17 04:50:30 -0700

    Update profile fragment and change color of bottom navigation icon when selected

---
+++ app/src/main/java/com/example/kat_app/Activities/EditAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.kat_app.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kat_app.R;
+
+public class EditAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account);
+    }
+}
--- app/src/main/java/com/example/kat_app/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/kat_app/Fragments/ProfileFragment.java
@@ -2,11 +2,9 @@ package com.example.kat_app.Fragments;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +14,8 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.kat_app.Activities.EditAccountActivity;
 import com.example.kat_app.Activities.LoginActivity;
-import com.example.kat_app.Activities.MainActivity;
 import com.example.kat_app.R;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
@@ -29,15 +27,17 @@ public class ProfileFragment extends Fragment {
     private ImageView ivProfileImage;
     private TextView tvName;
     private TextView tvUsername;
+    private TextView tvBalanceCount;
     private TextView tvProjectsCount;
     private TextView tvInvestmentsCount;
     private TextView tvDescription;
-    private ImageView ivLogout;
+    private ImageView ivSettings;
     private ImageView ivEdit;
 
     private static final String KEY_NAME = "name";
     private static final String KEY_PROFILE_IMAGE = "profile_image";
-    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_BIO = "bio";
+    private static final String KEY_BALANCE = "balance";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -49,23 +49,21 @@ public class ProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         setProfileInfo(view);
-        setLogoutButton(view);
+        setEditAccountButton(view);
     }
 
     private void setProfileInfo(View view) {
         // Find references for the views
         tvName = view.findViewById(R.id.tvName);
         tvUsername = view.findViewById(R.id.tvUsername);
-        tvProjectsCount = view.findViewById(R.id.tvProjectsCount);
-        tvInvestmentsCount = view.findViewById(R.id.tvInvestmentsCount);
-        tvDescription = view.findViewById(R.id.tvDescription);
+        tvBalanceCount = view.findViewById(R.id.tvBalanceCount);
         ivProfileImage = view.findViewById(R.id.ivProfileImage);
 
         ParseUser currUser = ParseUser.getCurrentUser();
 
         tvName.setText(currUser.getString(KEY_NAME));
         tvUsername.setText("@" + currUser.getUsername());
-        tvDescription.setText(currUser.getString(KEY_DESCRIPTION));
+        tvBalanceCount.setText("$" + currUser.getInt(KEY_BALANCE));
 
         ParseFile profileImage = currUser.getParseFile(KEY_PROFILE_IMAGE);
         if (profileImage != null) {
@@ -82,45 +80,16 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    private void setLogoutButton(View view) {
+    private void setEditAccountButton(View view) {
         // Find reference for the view
-        ivLogout = view.findViewById(R.id.ivLogout);
+        ivEdit = view.findViewById(R.id.ivEdit);
 
-        // Make the logout image clickable and open up dialog on click
-        ivLogout.setClickable(true);
-        ivLogout.setOnClickListener(new View.OnClickListener() {
+        // Set on-click listener for for image view to launch edit account activity
+        ivEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle("Log Out");
-                builder.setIcon(R.drawable.ic_alert);
-                builder.setMessage("Are you sure you want to log out?");
-                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ParseUser.logOut();
-                        final Intent intent = new Intent(getActivity(), LoginActivity.class);
-                        startActivity(intent);
-                        getActivity().finish();
-                        dialog.dismiss();
-
-                    }
-                });
-                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-
-                AlertDialog alert = builder.create();
-                alert.show();
-                Button negButton = alert.getButton(DialogInterface.BUTTON_NEGATIVE);
-                negButton.setTextColor(getResources().getColor(R.color.kat_orange_1));
-                Button posButton = alert.getButton(DialogInterface.BUTTON_POSITIVE);
-                posButton.setTextColor(getResources().getColor(R.color.kat_orange_1));
+                Intent intent = new Intent(getActivity(), EditAccountActivity.class);
+                startActivity(intent);
             }
         });
     }
