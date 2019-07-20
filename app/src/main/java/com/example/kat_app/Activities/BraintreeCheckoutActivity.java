bf0379c803b5a29caa7a49a4cd9612b9f11bc227
Timi Soetan <timisoetan@fb.com>
2019-07-20 16:13:24 -0700

    Switched from paypal sdk to braintree sdk for payment integration

---
+++ app/src/main/java/com/example/kat_app/Activities/BraintreeCheckoutActivity.java
@@ -0,0 +1,276 @@
+package com.example.kat_app.Activities;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.braintreepayments.api.dropin.DropInActivity;
+import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.braintreepayments.api.interfaces.HttpResponseCallback;
+import com.braintreepayments.api.internal.HttpClient;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+import com.example.kat_app.Models.Balance;
+import com.example.kat_app.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BraintreeCheckoutActivity extends AppCompatActivity {
+
+    private static final String TAG = "paymentExample";
+
+
+    private static final int REQUEST_CODE = 1234;
+    String API_GET_TOKEN="http://192.168.64.2/braintree/main.php";
+    String API_CHECKOUT="http://192.168.64.2/braintree/checkout.php";
+
+    String token,amount;
+    HashMap<String,String> paramsHash;
+    Button order;
+    EditText amount2;
+    private float currBalance;
+    private float addedCredits;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_braintree_checkout);
+
+        amount2 =(EditText)findViewById(R.id.amount);
+        order =(Button) findViewById(R.id.order);
+
+        new BraintreeCheckoutActivity.getToken().execute();
+
+
+
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitPayment();
+            }
+        });
+
+        ParseUser currUser = ParseUser.getCurrentUser();
+        queryBalance(currUser);
+    }
+
+    private void submitPayment(){
+        String payValue=amount2.getText().toString();
+
+        addedCredits = Float.parseFloat(amount2.getText().toString());
+
+        if(!payValue.isEmpty())
+        {
+            DropInRequest dropInRequest=new DropInRequest().clientToken(token);
+            startActivityForResult(dropInRequest.getIntent(this),REQUEST_CODE);
+        }
+        else
+            Toast.makeText(this, "Enter a valid amount for payment", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void sendPayments(){
+        RequestQueue queue= Volley.newRequestQueue(BraintreeCheckoutActivity.this);
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, API_CHECKOUT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.toString().contains("Successful")){
+                            Toast.makeText(BraintreeCheckoutActivity.this, "Payment Success", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(BraintreeCheckoutActivity.this, "Payment Failed", Toast.LENGTH_SHORT).show();
+                        }
+                        Log.d("Response",response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Err",error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                if(paramsHash==null)
+                    return null;
+                Map<String,String> params=new HashMap<>();
+                for(String key:paramsHash.keySet())
+                {
+                    params.put(key,paramsHash.get(key));
+                }
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params=new HashMap<>();
+                params.put("Content-type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        RetryPolicy mRetryPolicy=new DefaultRetryPolicy(0,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(mRetryPolicy);
+        queue.add(stringRequest);
+    }
+
+    private class getToken extends AsyncTask {
+        ProgressDialog mDailog;
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            HttpClient client=new HttpClient();
+            client.get(API_GET_TOKEN, new HttpResponseCallback() {
+                @Override
+                public void success(final String responseBody) {
+                    mDailog.dismiss();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            token=responseBody;
+                        }
+                    });
+                }
+
+                @Override
+                public void failure(Exception exception) {
+                    mDailog.dismiss();
+                    Log.d("Err",exception.toString());
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mDailog=new ProgressDialog(BraintreeCheckoutActivity.this,android.R.style.Theme_DeviceDefault_Light_Dialog);
+            mDailog.setCancelable(false);
+            mDailog.setMessage("Loading Wallet, Please Wait");
+            mDailog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Object o){
+            super.onPostExecute(o);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode== REQUEST_CODE){
+            if(resultCode==RESULT_OK)
+            {
+                DropInResult result=data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                PaymentMethodNonce nonce= result.getPaymentMethodNonce();
+                String strNounce=nonce.getNonce();
+                if(!amount2.getText().toString().isEmpty())
+                {
+                    amount=amount2.getText().toString();
+                    paramsHash=new HashMap<>();
+                    paramsHash.put("amount",amount);
+                    paramsHash.put("nonce",strNounce);
+
+                    Float newBalance = currBalance + addedCredits;
+                    ParseUser currUser = ParseUser.getCurrentUser();
+                    queryBalance(currUser, newBalance);
+
+                    sendPayments();
+                }
+                else {
+                    Toast.makeText(this, "Please enter a valid amount", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if(resultCode==RESULT_CANCELED)
+            {
+                Toast.makeText(this, "User canceled", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Exception error=(Exception)data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
+                Log.d("Err",error.toString());
+            }
+        }
+    }
+
+
+    //get balance from user pointer
+    protected void queryBalance(ParseUser currUser) {
+        final ParseQuery<Balance> balanceQuery = new ParseQuery<>(Balance.class);
+
+        balanceQuery.whereEqualTo("user", currUser);
+        balanceQuery.findInBackground(new FindCallback<Balance>() {
+            @Override
+            public void done(List<Balance> accounts, ParseException e) {
+                if (e != null) {
+                    Log.e("Query requests","Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+
+                Balance balance = accounts.get(0);
+                currBalance = balance.getAmount();
+            }
+        });
+    }
+
+    //update the user's balance
+    protected void queryBalance(ParseUser currUser, final float amount) {
+        final ParseQuery<Balance> balanceQuery = new ParseQuery<>(Balance.class);
+
+        balanceQuery.whereEqualTo("user", currUser);
+        balanceQuery.findInBackground(new FindCallback<Balance>() {
+            @Override
+            public void done(List<Balance> accounts, ParseException e) {
+                if (e != null) {
+                    Log.e("Query requests","Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+
+                Balance balance = accounts.get(0);
+                balance.put("amount", amount);
+                balance.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.d(TAG, "Balance updated!");
+                            setResult(RESULT_OK);
+                            onBackPressed();
+                        } else {
+                            Log.e(TAG, "Error while updating balance.");
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+}
