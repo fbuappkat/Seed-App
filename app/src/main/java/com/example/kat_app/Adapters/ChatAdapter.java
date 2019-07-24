3b42e1422a94459d74a1b0a0a93b8a39dd41e5dd
Timi Soetan <timisoetan@fb.com>
2019-07-24 15:16:15 -0700

    Chat ui completed

--- app/src/main/java/com/example/kat_app/Adapters/ChatAdapter.java
+++ app/src/main/java/com/example/kat_app/Adapters/ChatAdapter.java
@@ -2,7 +2,6 @@ package com.example.kat_app.Adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,96 +12,124 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.kat_app.Models.Message;
 import com.example.kat_app.R;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.util.List;
 
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
+public class ChatAdapter extends RecyclerView.Adapter {
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
 
-    private List<Message> mMessages;
     private Context mContext;
+    private List<Message> mMessageList;
     private String mUserId;
-    private ParseUser currUser;
     private ParseUser otherUser;
 
-    public ChatAdapter(Context context, String userId, List<Message> messages, ParseUser currUser, ParseUser otherUser) {
-        mMessages = messages;
-        this.mUserId = userId;
-        this.currUser = currUser;
-        this.otherUser = otherUser;
+    public ChatAdapter(Context context, List<Message> messageList, String userId, ParseUser otherUser) {
         mContext = context;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View contactView = inflater.inflate(R.layout.item_chat, parent, false);
-
-        ViewHolder viewHolder = new ViewHolder(contactView);
-        return viewHolder;
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Message message = mMessages.get(position);
-        final boolean isMe = message.getUserId() != null && message.getUserId().equals(mUserId);
-
-        if (isMe) {
-            holder.imageMe.setVisibility(View.VISIBLE);
-            holder.imageOther.setVisibility(View.GONE);
-            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
-        } else {
-            holder.imageOther.setVisibility(View.VISIBLE);
-            holder.imageMe.setVisibility(View.GONE);
-            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-        }
-
-        final ImageView profileView = isMe ? holder.imageMe : holder.imageOther;
-        if (isMe) {
-            Glide.with(mContext).load(currUser.getParseFile("profile_image").getUrl())
-                    .apply(RequestOptions.circleCropTransform())
-                    .into(profileView);
-        } else {
-            Glide.with(mContext).load(otherUser.getParseFile("profile_image").getUrl())
-                    .apply(RequestOptions.circleCropTransform())
-                    .into(profileView);
-        }
-        holder.body.setText(message.getBody());
-    }
-
-    // Create a gravatar image based on the hash value obtained from userId
-    private static String getProfileUrl(final String userId) {
-        String hex = "";
-        try {
-            final MessageDigest digest = MessageDigest.getInstance("MD5");
-            final byte[] hash = digest.digest(userId.getBytes());
-            final BigInteger bigInt = new BigInteger(hash);
-            hex = bigInt.abs().toString(16);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "https://www.gravatar.com/avatar/" + hex + "?d=identicon";
+        mMessageList = messageList;
+        this.mUserId = userId;
+        this.otherUser = otherUser;
     }
 
     @Override
     public int getItemCount() {
-        return mMessages.size();
+        return mMessageList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageOther;
-        ImageView imageMe;
-        TextView body;
+    // Determines the appropriate ViewType according to the sender of the message.
+    @Override
+    public int getItemViewType(int position) {
+        Message message = mMessageList.get(position);
 
-        public ViewHolder(View itemView) {
+        if (message.getMessageSender().getObjectId().equals(mUserId)) {
+            // If the current user is the sender of the message
+            return VIEW_TYPE_MESSAGE_SENT;
+        } else {
+            // If some other user sent the message
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+        }
+    }
+
+    // Inflates the appropriate layout according to the ViewType.
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_sent, parent, false);
+            return new SentMessageHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_received, parent, false);
+            return new ReceivedMessageHolder(view);
+        }
+
+        return null;
+    }
+
+    // Passes the message object to a ViewHolder so that the contents can be bound to UI.
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Message message =  mMessageList.get(position);
+
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_MESSAGE_SENT:
+                ((SentMessageHolder) holder).bind(message);
+                break;
+            case VIEW_TYPE_MESSAGE_RECEIVED:
+                ((ReceivedMessageHolder) holder).bind(message);
+        }
+    }
+
+    private class SentMessageHolder extends RecyclerView.ViewHolder {
+        TextView messageText, timeText;
+
+        SentMessageHolder(View itemView) {
             super(itemView);
-            imageOther = (ImageView) itemView.findViewById(R.id.ivProfileOther);
-            imageMe = (ImageView) itemView.findViewById(R.id.ivProfileMe);
-            body = (TextView) itemView.findViewById(R.id.tvBody);
+
+            messageText = (TextView) itemView.findViewById(R.id.tvSenderBody);
+            timeText = (TextView) itemView.findViewById(R.id.tvSenderTime);
+        }
+
+        void bind(Message message) {
+            messageText.setText(message.getBody());
+
+            // Format the stored timestamp into a readable String using method.
+            timeText.setText(message.getTime());
+        }
+    }
+
+    private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
+        TextView messageText, timeText, nameText;
+        ImageView profileImage;
+
+        ReceivedMessageHolder(View itemView) {
+            super(itemView);
+
+            messageText = (TextView) itemView.findViewById(R.id.tvReceivedBody);
+            timeText = (TextView) itemView.findViewById(R.id.tvReceivedTime);
+            nameText = (TextView) itemView.findViewById(R.id.tvReceivedName);
+            profileImage = (ImageView) itemView.findViewById(R.id.ivReceivedProfileImage);
+        }
+
+        void bind(Message message) {
+            messageText.setText(message.getBody());
+
+            // Format the stored timestamp into a readable String using method.
+            timeText.setText(message.getTime());
+            try {
+                nameText.setText(message.getMessageSender().fetchIfNeeded().getUsername());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            // Insert the profile image from the URL into the ImageView.
+            Glide.with(mContext).load(otherUser.getParseFile("profile_image").getUrl())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(profileImage);
         }
     }
 }
