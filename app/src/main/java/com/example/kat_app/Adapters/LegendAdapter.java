bd8bf1470886f7421d5a95bbd0a9d0f580c20e60
andrewkreisher <andrewkreisher@fb.com>
2019-08-05 11:29:42 -0700

    Legend in ProjectDetails not has accurate colors

--- app/src/main/java/com/example/kat_app/Adapters/LegendAdapter.java
+++ app/src/main/java/com/example/kat_app/Adapters/LegendAdapter.java
@@ -6,12 +6,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kat_app.Models.Request;
 import com.example.kat_app.R;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.List;
 
@@ -38,6 +40,8 @@ public class LegendAdapter extends RecyclerView.Adapter<LegendAdapter.ViewHolder
         PieEntry request = requests.get(position);
         holder.tvItem.setText(request.getLabel());
         holder.tvCost.setText(Float.toString(request.getValue()));
+        holder.btn.setBackgroundColor(ColorTemplate.JOYFUL_COLORS[position]);
+
     }
 
     @Override
@@ -50,6 +54,7 @@ public class LegendAdapter extends RecyclerView.Adapter<LegendAdapter.ViewHolder
         private TextView tvItem;
         private TextView tvCost;
         private ImageView ivColor;
+        private Button btn;
         View view;
 
         public ViewHolder(View itemView) {
@@ -57,7 +62,7 @@ public class LegendAdapter extends RecyclerView.Adapter<LegendAdapter.ViewHolder
             view = itemView;
             tvItem = itemView.findViewById(R.id.tvLegendItem);
             tvCost = itemView.findViewById(R.id.tvLegendCost);
-            ivColor = itemView.findViewById(R.id.ivColor);
+            btn = itemView.findViewById(R.id.btn);
         }
 
         //add in data for specific user's post
