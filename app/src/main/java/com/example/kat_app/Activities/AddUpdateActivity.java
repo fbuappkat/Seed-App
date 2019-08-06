1aae3be9435e08f4bf75bb990768e58ae9a660c3
Timi Soetan <timisoetan@fb.com>
2019-08-06 13:34:43 -0700

    fix blurry image captured from camera

--- app/src/main/java/com/example/kat_app/Activities/AddUpdateActivity.java
+++ app/src/main/java/com/example/kat_app/Activities/AddUpdateActivity.java
@@ -1,13 +1,20 @@
 package com.example.kat_app.Activities;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,6 +29,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.azoft.carousellayoutmanager.CarouselLayoutManager;
 import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
@@ -32,6 +40,7 @@ import com.example.kat_app.Models.Comment;
 import com.example.kat_app.Models.Project;
 import com.example.kat_app.Models.Update;
 import com.example.kat_app.R;
+import com.facebook.login.Login;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -43,6 +52,7 @@ import org.json.JSONArray;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,6 +61,8 @@ import butterknife.ButterKnife;
 
 /* AddUpdateActivity allows the user to add an update about a specific project. */
 public class AddUpdateActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
 
     private final String TAG = "Add Update Activity";
 
@@ -78,6 +90,9 @@ public class AddUpdateActivity extends AppCompatActivity implements View.OnClick
     private Bitmap yourbitmap;
     private Bitmap resized;
     private ProgressDialog LoadingBar;
+
+    ContentValues values;
+    Uri imageUri = null;
 
     public final String photoFileName = "photo.jpg";
     private ParseFile photoFile;
@@ -139,7 +154,17 @@ public class AddUpdateActivity extends AppCompatActivity implements View.OnClick
             public void onClick(DialogInterface dialog, int item) {
 
                 if (options[item].equals("Take Photo")) {
+                    values = new ContentValues();
+                    values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                    values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+                     if (checkPermissionWRITE_EXTERNAL_STORAGE(AddUpdateActivity.this)) {
+                         imageUri = getContentResolver().insert(
+                                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                     } else {
+                         return;
+                     }
                     Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    takePicture.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
                     startActivityForResult(takePicture, 0);
 
                 } else if (options[item].equals("Choose from Gallery")) {
@@ -152,6 +177,71 @@ public class AddUpdateActivity extends AppCompatActivity implements View.OnClick
             }
         });
         builder.show();
+    }
+
+    public boolean checkPermissionWRITE_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+                    Toast.makeText(AddUpdateActivity.this, "GET PHOTO ACCESS DENIED",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
     }
 
     private void setAddUpdateButton() {
@@ -202,7 +292,13 @@ public class AddUpdateActivity extends AppCompatActivity implements View.OnClick
                 case 0:
                     if (resultCode == RESULT_OK && data != null) {
                         rvImages.setVisibility(View.VISIBLE);
-                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        Bitmap selectedImage = null;
+                        try {
+                            selectedImage = MediaStore.Images.Media.getBitmap(
+                                    getContentResolver(), imageUri);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                         ParseFile convertedImage = conversionBitmapParseFile(selectedImage);
                         images.add(convertedImage);
                         photos.add(selectedImage);
